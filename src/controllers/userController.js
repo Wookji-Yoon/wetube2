ceda459e1b1083f96b61e7ea3542145928eb651d
Wookji-Yoon <Wookji-Yoon@github.com>
2022-01-12 00:53:26 +0900

    #7.6 Made Login Form!

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,75 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username or email is already taken",
+    });
+  }
+
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
 };
+
+//로그인 페이지입니다//
+export const getLogin = (req, res) => {
+  return res.render("login.pug", { pageTitle: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const pageTitle = "Login";
+
+  //계정 있는지 확인
+  if (!user) {
+    return res.status(400).render("login.pug", {
+      pageTitle,
+      errorMessage: "There is no such an account",
+    });
+  }
+
+  //PW 확인
+  const checkPW = await bcrypt.comapare(password, user.password);
+  if (!checkPW) {
+    return res.status(400).render("login.pug", {
+      pageTitle,
+      errorMessage: "Wrong Password",
+    });
+  }
+
+  //로그인 시켜야해!!
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
